31b95c434e1762682b571fcf6534ea2a1f0bb1e5
enigma <ninadthakare458@gmail.com>
2021-05-16 14:57:55 +0530

    Formatting AppDatabase annotation

--- app/src/main/java/com/androidtraining/personalrutineapp/dataBase/AppDatabase.kt
+++ app/src/main/java/com/androidtraining/personalrutineapp/dataBase/AppDatabase.kt
@@ -1,25 +1,26 @@
 package com.androidtraining.personalrutineapp.dataBase
 
+import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import android.content.Context
 import com.androidtraining.personalrutineapp.converter.DateTypeConverter
 import com.androidtraining.personalrutineapp.converter.ListConverter
-import com.androidtraining.personalrutineapp.dao.*
+import com.androidtraining.personalrutineapp.dao.ExerciseDao
+import com.androidtraining.personalrutineapp.dao.GenderDao
+import com.androidtraining.personalrutineapp.dao.RoutineDao
+import com.androidtraining.personalrutineapp.dao.TraineeDao
 import com.androidtraining.personalrutineapp.entity.Exercise
 import com.androidtraining.personalrutineapp.entity.Gender
 import com.androidtraining.personalrutineapp.entity.Routine
 import com.androidtraining.personalrutineapp.entity.Trainee
 
-@Database(entities = [
-    Exercise::class,
-    Gender::class,
-    Routine::class,
-    Trainee::class
-                     ],
-    version = 1, exportSchema = false)
+@Database(
+    entities = [Exercise::class, Gender::class, Routine::class, Trainee::class],
+    version = 1,
+    exportSchema = false
+)
 @TypeConverters(DateTypeConverter::class, ListConverter::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun exerciseDao(): ExerciseDao
